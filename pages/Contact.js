216e41846802f81e089d31dd9ea008baa21d962f
Bijoy Valsan <bijoy.valsan@smava.de>
2021-03-08 10:50:34 +0100

    contact info

--- pages/Contact.js
+++ pages/Contact.js
@@ -1,5 +1,32 @@
+import React from "react";
+import { Card } from "./../components";
+
 const Contact = () => {
-  return <div>Contact</div>
+  return <><Card>
+    <div className="contact_us">
+      <h1>CONTACT</h1>
+      <div style={{ display: "flex" }}>
+        <div style={{ paddingRight: "25px" }}>
+          <p> <b>Burger Spot Private Limited </b></p>
+          <p>  24/61 Cochin university road </p>
+          <p>  South kalamassery </p>
+          <p>  Ernakulam </p>
+          <p>  Kerala 682022 </p>
+        </div>
+        <div style={{ paddingRight: "25px" }}>
+          <span>
+            <p> Mobile: <b>9207421111</b></p>
+          </span>
+
+          <span>
+            <p> Email: <b>info@burgerspot.in</b></p>
+          </span>
+
+        </div>
+
+      </div>
+    </div>
+  </Card></>
 }
 
 export default Contact;
--- pages/Menu.js
+++ pages/Menu.js
@@ -1,7 +1,17 @@
 const Menu = () => {
   return <div>
+    <div style={{marginBottom:"10px", width:"200px", marginLeft: "auto"}}>
+      <a title="download menu" className="download_link" target="_blank" href="/pdf/burgerspot_menu.pdf" tabIndex="0">
+        <svg style={{ width: "24px", height: "24px" }} viewBox="0 0 24 24">
+        <path fill="currentColor" d="M5,20H19V18H5M19,9H15V3H9V9H5L12,16L19,9Z" />
+        </svg>
+        <span>
+          Download Menu
+        </span>
+      </a>
+    </div>
     <img src="/images/menu-1.jpg" alt="BurgerSpot Menu" width="100%" height="100%" />
-    <img src="/images/menu-2.jpg" alt="BurgerSpot Menu" width="100%" height="100%"  />
+    <img src="/images/menu-2.jpg" alt="BurgerSpot Menu" width="100%" height="100%" />
   </div>
 }
 
