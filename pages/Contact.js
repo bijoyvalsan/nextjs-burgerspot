ed88968523c74e9d654cebcf6f60eae29405264c
Bijoy Valsan <bijoy.valsan@smava.de>
2021-03-08 12:44:46 +0100

    contact fix on mobile

--- pages/Contact.js
+++ pages/Contact.js
@@ -5,15 +5,15 @@ const Contact = () => {
   return <><Card>
     <div className="contact_us">
       <h1>CONTACT</h1>
-      <div style={{ display: "flex" }}>
-        <div style={{ paddingRight: "25px" }}>
+      <div style={{ display: "flex", flexWrap:"wrap" }}>
+        <div style={{ paddingRight: "25px", minWidth:"200px" }}>
           <p> <b>Burger Spot Private Limited </b></p>
           <p>  24/61 Cochin university road </p>
           <p>  South kalamassery </p>
           <p>  Ernakulam </p>
           <p>  Kerala 682022 </p>
         </div>
-        <div style={{ paddingRight: "25px" }}>
+        <div style={{ paddingRight: "25px",minWidth:"200px"  }}>
           <span>
             <p> Mobile: <b>9207421111</b></p>
           </span>
